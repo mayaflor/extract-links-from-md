06e3b6d04bd3b74efa80cd1d1f20171b21ddedf6
Maya Flor <maya.r.rebelo@gmail.com>
2018-09-03 20:19:46 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+module.exports.getLinksFromMd = function getLinksFromMd(text) {
+
+  if (text === "") {
+    throw new Error ("No Parameter found")
+  };
+  if (typeof text === "number"){
+    throw new Error ("Parameter must contain letters")
+  };
+  if (url === null){
+    return "";
+  }
+  if (typeof text === "string") {
+    let url = text.match(/(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+/g);
+    let alt = text.match(/(?<=\[).*?(?=\])/g);
+  
+    let urlObj = {}
+    let links = []
+
+    if (url !== null){
+      url.map((url, i) => {
+        urlObj = {href: url, text: alt[i]}
+        links.push(urlObj);
+      });
+    };
+
+    return links
+  } 
+};
