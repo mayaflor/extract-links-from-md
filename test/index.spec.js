d321a0fbc50b630fba1307245921739a0ad9200c
Maya Flor <maya.r.rebelo@gmail.com>
2018-09-04 17:31:01 -0300

    all tests parring

--- test/index.spec.js
+++ test/index.spec.js
@@ -24,16 +24,16 @@ describe('getLinksFromMd()', () => {
   describe('When given a string with one link', () => {
     it('Should return a link', () => {
       const oneLink = "Oi você quer entrar no site [google](www.google.com) ?";
-      expect(getLinks(oneLink)).to.equal([{href: 'www.google.com', text: 'google'}]);
+      expect(getLinks(oneLink)).to.deep.equal([{href: 'www.google.com', text: 'google'}]);
       // expect(() => (getLinks("Oi você quer entrar no site [google](www.google.com) ?"))).to.equal("[{href: 'www.google.com', text: 'google'}]")
     });
   });
   describe('When given a string with three links', () => {
     it('Should return three inks', () => {
       const threeLink = "Oi você quer entrar no site [google](www.google.com) , [twitter](www.twitter.com) ou [facebook](www.facebook.com) ?";
-      expect(getLinks(threeLink)).to.equal([ { href: 'www.google.com', text: 'google' },
-      { href: 'www.twitter.com', text: 'twitter' },
-      { href: 'www.facebook.com', text: 'facebook' } ]);
+      expect(getLinks(threeLink)).to.deep.equal([{href: 'www.google.com', text: 'google'},
+      {href: 'www.twitter.com', text: 'twitter'},
+      {href: 'www.facebook.com', text: 'facebook'}]);
     });
   });
 });
